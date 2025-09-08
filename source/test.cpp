f646ae3a2a58e1c87eee60467b5eff115d8dde87
Exityx <gdlasorum@gmail.com>
2025-09-08 19:13:39 +0300

    Added source/test.cpp

---
+++ source/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "../include/root.h"
+#include "../include/widget.h"
+
+int main(void)
+{
+    using namespace std;
+    Root my_root("My program");
+    
+    Widget w(5, 4, 150, 200);
+    Widget s(20, 31, 400, 100);
+
+    cout << "Print classes:\n\n";
+    cout << my_root << endl;
+    cout << w << endl;
+    cout << endl;
+
+    my_root.add_widget(w);
+
+    cout << "Test exists widgets:\n\n";
+    cout.setf(ios_base::boolalpha);
+    cout << "Widget 'w' is exists?: " << my_root.widget_is_exists(w) << endl;
+    cout << "Widget 's' is exists?: " << my_root.widget_is_exists(s) << endl;
+    return 0;
+}
