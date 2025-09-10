ba9ffd2b81fefd06edd978485058a60abae3d96a
Exityx <gdlasorum@gmail.com>
2025-09-10 20:36:28 +0300

    test: Testing classes

--- source/test.cpp
+++ source/test.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
-#include "../include/root.h"
-#include "../include/widget.h"
+#include <root.h>
+#include <widget.h>
+#include <text.h>
 
 int main(void)
 {
     using namespace std;
-    Root my_root("My program");
-    
-    Widget w(5, 4, 150, 200);
-    Widget s(20, 31, 400, 100);
-    Widget copy = w;
+    Root app("Test");
+    Widget main;
+    Text text("Hello, World!");
 
-    cout << "Print classes:\n\n";
-    cout << my_root << endl;
-    cout << w << endl;
-    cout << endl;
-
-    my_root.add_widget(w);
-
-    cout << "Test exists widgets:\n\n";
-    cout.setf(ios_base::boolalpha);
-    cout << "Widget 'w' is exists?: " << my_root.widget_is_exists(w) << endl;
-    cout << "Widget 's' is exists?: " << my_root.widget_is_exists(s) << endl;
-    cout << "Widget 'w' equal 's'?: " << (w == s) << endl;
-    cout << "Widget 'w' equal 'copy'?: " << (w == copy) << endl;
-    cout << "Widget 'copy' is exists?: " << my_root.widget_is_exists(copy) << endl;
+    main.add(text);
+    main.add(Text("Hi"));
+    app.add_widget(main);
+    app.draw();
     return 0;
 }
