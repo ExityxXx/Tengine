6403621bc18a35f7e7a589235565328788f9f323
Exityx <gdlasorum@gmail.com>
2025-09-10 20:31:02 +0300

    feat(class): several functions have been changed and the draw() function has been added

--- include/root.h
+++ include/root.h
@@ -5,20 +5,20 @@
 #define TENGINE_ROOT_H_
 #include <ostream>
 #include <vector>
-#include "widget.h"
+#include <widget.h>
 
 class Root
 {
 private:
     const char *title;
     std::vector<Widget> widgets;
-    
 public:
-    Root(const char *new_title = "Default Root");
+    explicit Root(const char *new_title = "Default Root");
     void reset(const char *new_title = "Default Root");
-    const char *get_title() const;
-    bool widget_is_exists(const Widget &widget);
+    const char *to_string() const;
+    bool widget_is_exists(const Widget &widget) const;
     void add_widget(const Widget &widget);
+    void draw() const;
     friend std::ostream &operator<<(std::ostream &os, const Root &root);
 };
 
