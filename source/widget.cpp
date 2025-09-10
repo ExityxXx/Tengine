e3a19a2293be3dba63c936c9e9cc37f202d419de
Exityx <gdlasorum@gmail.com>
2025-09-10 20:37:38 +0300

    feat(widget): Add constructors, getters and others.

--- source/widget.cpp
+++ source/widget.cpp
@@ -1,4 +1,4 @@
-#include "../include/widget.h"
+#include <widget.h>
 
 Widget::Widget(
     int new_x,
@@ -13,21 +13,54 @@ Widget::Widget(
     width = new_width;
 }
 
-bool Widget::operator==(const Widget &widget)
+void Widget::reset(
+    int new_x,
+    int new_y,
+    int new_height,
+    int new_width
+)
+{
+    x = new_x;
+    y = new_y;
+    height = new_height;
+    width = new_width;
+}
+
+int Widget::get_height() const
+{
+    return height;
+}
+
+int Widget::get_width() const
+{
+    return width;
+}
+
+bool Widget::operator==(const Widget &widget) const
 {
     return (
-            (this->x == widget.x) &&
-            (this->y == widget.y) &&
-            (this->width == widget.width) &&
-            (this->height == widget.height)
+        (x == widget.x) &&
+        (y == widget.y) &&
+        (width == widget.width) &&
+        (height == widget.height)
             );
+}
+
+void Widget::add(const Text &text)
+{
+    text_arr.push_back(text);
+}
+
+std::vector<Text> Widget::get_text_arr() const
+{
+    return text_arr;
 }
 
 std::ostream &operator<<(std::ostream &os, const Widget &widget)
 {
-    os << "x = " << widget.x << std::endl;
-    os << "y = " << widget.y << std::endl;
-    os << "height = " << widget.height << std::endl;
-    os << "width = " << widget.width << std::endl;
+    os << "Widget(" << widget.x << ", "
+                    << widget.y << ", "
+                    << widget.height << ", "
+                    << widget.width << ")";
     return os;
 }
