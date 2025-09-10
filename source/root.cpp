0abbf69c0ae6daf7dcef114632a37d5d92390adf
Exityx <gdlasorum@gmail.com>
2025-09-10 20:35:37 +0300

    feat: Change in functionality

--- source/root.cpp
+++ source/root.cpp
@@ -2,17 +2,53 @@
 // root.cpp
 
 #include <iostream>
-#include "../include/root.h"
+#include <root.h>
+#include <vector>
+#include <text.h>
 
-bool Root::widget_is_exists(const Widget &widget)
+Root::Root(const char *new_title)
+{ 
+    title = new_title;
+}
+
+void Root::reset(const char *new_title)
+{
+    title = new_title;
+}
+
+void Root::add_widget(const Widget &widget)
+{
+    widgets.push_back(widget);
+}
+
+bool Root::widget_is_exists(const Widget &widget) const
 {
     for (int i = 0; i < widgets.size(); ++i)
         if (widgets[i] == widget) return true;
     return false;
 }
 
-Root::Root(const char *new_title) { title = new_title; }
-void Root::reset(const char *new_title) { title = new_title; }
-void Root::add_widget(const Widget &widget) { widgets.push_back(widget); }
-const char *Root::get_title() const { return title; }
-std::ostream &operator<<(std::ostream &os, const Root &root) { os << root.title; return os; }
+const char *Root::to_string() const
+{
+    return title;
+}
+
+std::ostream &operator<<(std::ostream &os, const Root &root)
+{
+    os << "Root(" << root.title << ")";
+    return os;
+}
+
+void Root::draw() const
+{
+    using std::cout;
+    using std::endl;
+    using std::vector;
+
+    for (int i = 0; i < widgets.size(); ++i)
+    {
+        vector<Text> text_arr = widgets[i].get_text_arr();
+        for (int j = 0; j < text_arr.size(); ++j) cout << text_arr[j].to_string() << endl;
+    }
+
+}
